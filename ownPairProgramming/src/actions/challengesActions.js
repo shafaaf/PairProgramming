c82b687d864ad6f8153a393d62b411e01cbee008
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-08-31 19:18:10 -0400

    first commit

---
+++ ownPairProgramming/src/actions/challengesActions.js
@@ -0,0 +1,8 @@
+export function getChallenges() {
+  const challenges = fetch(`${process.env.API_HOST}/api/v1/challenges`).then(res => {
+    return res.json()
+  }).then(res => {
+    return res
+  })
+  return {type: "GET_CHALLENGES", payload: challenges}
+}
